13a5c0d9f2b11b69bfd80cbf3bcf2d05a19650cd
Sahed Wahedi <sahed.w@hotmail.de>
2023-02-09 11:09:01 +0100

    add toggle functionality on both cards in the homepage

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+const bookmarkButton1 = document.querySelector('[data-js="bookmark-button1"]');
+const bookmarkButton2 = document.querySelector('[data-js="bookmark-button2"]');
 
-bookmarkButton.addEventListener("click", () => {});
+bookmarkButton1.addEventListener("click", () => {
+  bookmarkButton1.classList.toggle("checked");
+});
+
+bookmarkButton2.addEventListener("click", () => {
+  bookmarkButton2.classList.toggle("checked");
+});
