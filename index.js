7ec2d36adadf946ecc7ef63aa0564246fdc29237
Sahed Wahedi <sahed.w@hotmail.de>
2023-02-09 11:30:34 +0100

    add toggle functionality for answer

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const bookmarkButton1 = document.querySelector('[data-js="bookmark-button1"]');
 const bookmarkButton2 = document.querySelector('[data-js="bookmark-button2"]');
 
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answer = document.querySelector('[data-js="answer-text"]');
+const question = document.querySelector('[data-js="question-text"]');
+
 bookmarkButton1.addEventListener("click", () => {
   bookmarkButton1.classList.toggle("checked");
 });
 
 bookmarkButton2.addEventListener("click", () => {
   bookmarkButton2.classList.toggle("checked");
+});
+
+answerButton.addEventListener("click", () => {
+  answer.classList.toggle("hidden");
+  question.classList.toggle("hidden");
 });
