ee167c4ffd54c03a74177d7b18325bf207ce43a9
Sahed Wahedi <sahed.w@hotmail.de>
2023-02-11 23:06:55 +0100

    add placeholders and inputs in js file

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const answerButton = document.querySelector('[data-js="answer-button"]');
 const answer = document.querySelector('[data-js="answer-text"]');
 const question = document.querySelector('[data-js="question-text"]');
 
+const questionInput = document.querySelector('[data-js="question"]');
+const answerInput = document.querySelector('[data-js="answer"]');
+const submitButton = document.querySelector('[data-js="submit-button"]');
+
 bookmarkIcon.addEventListener("click", () => {
   bookmarkIcon.classList.toggle("card__bookmark--checked");
 });
