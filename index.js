f548e7b9b90ed14790afe854d8e9a4caa5d34fd2
Sahed Wahedi <sahed.w@hotmail.de>
2023-02-12 13:56:11 +0100

    fixes

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
-
-console.log(bookmarkIcon);
-
 const answerButton = document.querySelector('[data-js="answer-button"]');
 const answer = document.querySelector('[data-js="answer-text"]');
 const question = document.querySelector('[data-js="question-text"]');
 
 const questionInput = document.querySelector('[data-js="question"]');
+const remainingCharacters = document.querySelector(
+  '[data-js="remaining-characters"]'
+);
 const answerInput = document.querySelector('[data-js="answer"]');
 const submitButton = document.querySelector('[data-js="submit-button"]');
+
+console.log("Hey");
 
 bookmarkIcon.addEventListener("click", () => {
   bookmarkIcon.classList.toggle("card__bookmark--checked");
