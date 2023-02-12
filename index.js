d8ed4ebaf665660bf6d0c52a785e79db90743de4
Sahed Wahedi <sahed.w@hotmail.de>
2023-02-12 13:06:10 +0100

    fix issue with js file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const bookmarkIcon = document.querySelector('[data-js="bookmark-icon"]');
 
+console.log(bookmarkIcon);
+
 const answerButton = document.querySelector('[data-js="answer-button"]');
 const answer = document.querySelector('[data-js="answer-text"]');
 const question = document.querySelector('[data-js="question-text"]');
